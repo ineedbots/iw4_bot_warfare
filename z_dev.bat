40629600f7cb7f4f748f433e2d9428c5cbf3f6b2
Your Name <you@example.com>
2020-08-28 16:44:51 -0600

    a

--- z_dev.bat
+++ z_dev.bat
@@ -1,1 +1,1 @@
-start iw4x.exe -nosteam -console +set r_fullscreen "0" +set fs_game "mods/dev" +set developer "1" +set developer_script "1" +set cg_drawfps "4" +set sv_cheats "1" +set drawlagometer "1" +set scr_game_spectatetype "2" +set r_mode "1024x768"  +devmap mp_rust
+start iw4x.exe -nosteam -dump -console +set r_fullscreen "0" +set fs_game "mods/dev" +set developer "1" +set developer_script "1" +set cg_drawfps "4" +set sv_cheats "1" +set drawlagometer "1" +set scr_game_spectatetype "2" +set r_mode "1024x768"  +devmap mp_rust
