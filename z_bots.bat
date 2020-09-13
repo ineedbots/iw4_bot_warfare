1bfbfb4a3a55b0c0e17e9ec5ac1698e3ffaac62a
Your Name <you@example.com>
2020-09-13 17:55:07 -0600

    k

--- z_bots.bat
+++ z_bots.bat
@@ -1,1 +1,1 @@
-start iw4x.exe -nosteam -console +set r_fullscreen "0" +set r_mode "1024x768" +set drawlagometer "0" +set cg_drawfps "0" +set fs_game "mods/dev" +set bots_manage_fill "4" +set ui_mapname "mp_rust" +set scr_game_spectatetype "2" +set scr_war_scorelimit "0" +map mp_rust
+start iw4x.exe -nosteam -scriptablehttp -console +set r_fullscreen "0" +set r_mode "1024x768" +set drawlagometer "0" +set cg_drawfps "0" +set fs_game "mods/dev" +set bots_manage_fill "4" +set ui_mapname "mp_rust" +set scr_game_spectatetype "2" +set scr_war_scorelimit "0" +map mp_rust
