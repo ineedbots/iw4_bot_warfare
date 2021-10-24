af35ec3a8f8bfbf8f637c01a3ed1cf213ee8f1ec
ineedbots <ineedbots@outlook.com>
2021-10-24 02:17:14 -0600

    Update to use findstr

--- z_server_pvbrust_watchdog.bat
+++ z_server_pvbrust_watchdog.bat
@@ -1,6 +1,10 @@
 @echo off
-::Name of the server's title
-set name=Bot Warfare Players vs bots 24/7 Rust 10x
+::Name
+set name=IW4x Bot Warfare PvB Rust
+::Exe of the server
+set server_exe=iw4x.exe
+::The regex search for the window name of the server
+set server_title_regex=Bot Warfare Players vs bots 24/7 Rust 10x
 ::Only change this when you don't want to keep the bat files in the game folder. MOST WON'T NEED TO EDIT THIS!
 set gamepath=%cd%
 ::Rate to check if server is hung
@@ -25,6 +29,10 @@ for /f "delims=" %%i in ('"forfiles /p "%log_path%" /m "%log_file%" /c "cmd /c e
 
 	if "%modif_time_temp%" == "%modif_time%" (
 		echo "(%date%)  -  (%time%) WARNING: %name% server hung, killing server..."
-		taskkill /FI "WINDOWTITLE eq %name%" /F
+		::https://stackoverflow.com/questions/26552368/windows-batch-file-taskkill-if-window-title-contains-text
+		for /f "tokens=2 delims=," %%a in ('
+			tasklist /fi "imagename eq %server_exe%" /v /fo:csv /nh
+			^| findstr /r /c:"%server_title_regex%"
+		') do taskkill /pid %%a /f
 	)
 goto Server
